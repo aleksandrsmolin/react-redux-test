1f0c4b5c09f0d84b745962806ce226634dbd0e0e
Oleksandr Smolin <krakensham@gmail.com>
2017-05-26 22:00:53 +0300

    New components to test add

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Link from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+ReactDOM.render(<Link />, document.getElementById('root'));
+registerServiceWorker();
